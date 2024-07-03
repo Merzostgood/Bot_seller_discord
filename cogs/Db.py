15764ed1191dcfc23b27f22829955ca13c865f6f
Tyher228 <fanatrrobzi@gmail.com>
2024-07-03 20:13:48 +0800

    Renamed file v4.0

--- cogs/Db.py
+++ cogs/Db.py
@@ -2,13 +2,13 @@ import json, sys
 sys.path.append("..")
 
 async def JSONUpdate(data):
-    with open("database.json", "w") as f:
-        f.write(json.dumps(data, indent=4))
+    with open("database.json", "w", encoding='utf8') as f:
+        f.write(json.dumps(data, indent=4, ensure_ascii=False))
     f.close()
     return data
 
 async def reader():
-    with open("database.json", "r") as f:
+    with open("database.json", "r", encoding='utf8') as f:
         data = json.loads(f.read())
     f.close()
     return data
